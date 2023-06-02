1b8d722af21e19dadc0c304f0cf1691833a9ab1b
Andreas Knollmann <a.knollmann@seak.dev>
2023-06-02 07:59:18 +0200

    added Magento modules list

--- modules.sh
+++ modules.sh
@@ -21,7 +21,7 @@ active=
 source "${currentPath}/../core/prepare-parameters.sh"
 
 if [[ "${active}" == 1 ]]; then
-  "${currentPath}/../core/script/run-quiet.sh" "webServer" "${currentPath}/third-party-modules/web-server.sh" --active | sort -n
+  "${currentPath}/../core/script/run-quiet.sh" "webServer" "${currentPath}/modules/web-server.sh" --active | sort -n
 else
-  "${currentPath}/../core/script/run-quiet.sh" "webServer" "${currentPath}/third-party-modules/web-server.sh" | sort -n
+  "${currentPath}/../core/script/run-quiet.sh" "webServer" "${currentPath}/modules/web-server.sh" | sort -n
 fi
